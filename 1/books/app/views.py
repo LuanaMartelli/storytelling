2a9f11f206653a4a4d9642a99f57d37304adc003
LuanaMartelli <lou.telli@gmail.com>
2024-11-01 17:31:11 +0100

    add template

--- 1/books/app/views.py
+++ 1/books/app/views.py
@@ -4,11 +4,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
-def home(request):
-
-        return HttpResponse('My First Home')
-
+import os
 
 def index(request):
-        return render(request, "index.html")
+
+        base = os.path.dirname(__file__)
+
+        files = {
+                'test': os.path.join(base, 'static/text/test.txt'),
+                'test2':os.path.join(base, 'static/text/test2.txt'),
+        }
+
+        content = {}
+        for key, file_path in files.items():
+                with open(file_path, 'r') as file:
+                        content[key] = file.read()
+
+        return render(request, "index.html", content)
